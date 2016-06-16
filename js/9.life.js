01f3183f94dd89b6da89b53b7c3cbb2329b569bb
zhufengnodejs <zhufengnodejs@126.com>
2016-06-16 17:28:32 +0800

    组件生命周期和如何移除组件

--- js/9.life.js
+++ js/9.life.js
@@ -12,6 +12,12 @@ var Message = React.createClass({
     componentWillMount:function(){
         console.log('3. componentWillMount 将要被加载');
     },
+    kill:function(){
+        ReactDOM.unmountComponentAtNode(document.querySelector('#app'));
+    },
+    componentWillUnmount:function(){
+        console.log('8.componentWillUnmount');
+    },
     //4.挂载 其实就是把组件插入DOM
     render:function(){
         console.log('4. render 渲染');
@@ -19,12 +25,18 @@ var Message = React.createClass({
             <div>
                 <h1>{this.state.count}</h1>
                 <button onClick={this.handleClick}>增加</button>
+                <button onClick={this.kill}>移除组件</button>
+
+                <SubMessage count={this.state.count}></SubMessage>
             </div>
         )
     },
     //当状态发生改变的时候，询问是否需要更新
     shouldComponentUpdate:function(){
         console.log('5. shouldComponentUpdate 询问是否需要更新');
+        if(this.state.count >=10){
+            return false;
+        }
         return true;//返回true表示可以更新视图
     },
     //组件将要被更新
@@ -40,5 +52,23 @@ var Message = React.createClass({
     }
 });
 
+var SubMessage = React.createClass({
+    //子组件将要收到新的属性
+    componentWillReceiveProps:function(){
+        console.log('子组件','componentWillReceiveProps');
+    },
+    //是否应该更新组件
+    shouldComponentUpdate:function(){
+        console.log('5. shouldComponentUpdate 询问是否需要更新');
+        if(this.props.count >=5){
+            return false;
+        }
+        return true;//返回true表示可以更新视图
+    },
+    render:function(){
+        return <div>子组件:{this.props.count}</div>
+    }
+
+});
 
 ReactDOM.render(<Message></Message>,document.querySelector('#app'))
